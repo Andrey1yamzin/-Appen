1109b24ec7a598054cf2d6e35d59dab54fe70c02
andrey <1yamzin98@mail.ru>
2022-02-16 23:56:52 +0700

    donwload project

---
+++ main.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const url = require('url'); 
+const {app, BrowserWindow} = require('electron');
+const { dirname } = require('path');
+
+let win;
+
+function createWindow(){
+    win = new BrowserWindow({
+        width:980, 
+        height:800, 
+        icon: __dirname + '/icons/icon.png'
+    });
+
+    win.loadURL(url.format({
+        pathname:path.join(__dirname, 'index.html'),
+        protocol:'file',
+        slashes:true
+    }));
+    //потом закоментить
+    win.webContents.openDevTools();
+      //потом закоментить
+    win.on('closed', function(){
+        win = null; 
+    }) 
+}
+
+app.on('ready', createWindow);
