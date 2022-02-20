b20167b861ff03df52bc706053e6565cf6e7de4b
andrey <1yamzin98@mail.ru>
2022-02-20 22:39:18 +0700

    did nothing

--- app.js
+++ app.js
@@ -4,3 +4,13 @@
 //     let key = this.value;
 //     console.log(key);
 // });
+
+//кнопка сброса
+let btnReload = document.querySelector('.btn-reload');
+//кнопка поиска
+let btnSearch = document.querySelector('.btn-search');
+// ключенвые слова по которым должен осуществляться поиск
+let listOwnership = document.querySelector('.content--ownership');
+let listRoom = document.querySelector('.content--room');
+let listArea = document.querySelector('.content--area');
+let listSquare = document.querySelector('.content--square');
