81c496f7b4fbc97094ac53c9ff203e0a07692a95
andrey <1yamzin98@mail.ru>
2022-03-17 23:41:19 +0700

    createparsejson

--- readfile.js
+++ readfile.js
@@ -1,55 +1,71 @@
 
-let data = request('http://localhost:3000');
-async function request(url, method = 'GET', data = null){
-    
-try{
-    const headers = {};
-    let body
-    if(data){
-        headers['Content-Type'] = 'application/json'
-        body = JSON.stringify(data)
-    }
+fetch('http://localhost:3000')
+.then(res=>res.json())
+.then(function(data){
+    record(data.posts);
+}).catch(err=>console.log('ERROR'))
 
-   const response = await fetch(url, {
-        method,
-        headers,
-        body
-    })
-    return await response.json();  
 
-} catch(e){
-    console.warn('Error');
-}
+function record(database){
+
+
+    // for(key in database){
+    //     for(let i = 0; i < database.length; i++){
+    //         console.log(key)
+    //         };
+    // }
+console.log(database);
+
+// database.forEach(el => {
+//     document.querySelector('.data-wrapper').innerHTML = `<table class="data-base"></table>`;
+//   for(key in el){
+//     let colum = document.createElement('theader');
+//     colum.innerHTML = `<td>${key}</td>`;
+//     document.querySelector('.data-base').appendChild(colum);
+
+//     console.log(key)
+//      for(let i = 0; i<database.length; i++){
+      
+//       }}
+// });
+
+
 };
 
+   
 
 
 
-let parse ;
-data.then(function (f1) {
-parse = (f1.posts);
 
 
-for(key in parse){
+
+
+
+// let parse ;
+// data.then(function (f1) {
+// // return parse = (f1.posts);
+// console.log(f1.posts)
+// })
+
+// console.log(parse)
+// for(key in parse){
     
-}
-parse.forEach(function(element, index, array) {
-    record(element, index);
-});
-});
-
-function record(el, int){
-document.querySelector('.data-wrapper').innerHTML = `<table class="data-base"></table>`;
-for(key in el){
-    let colum = document.createElement('td');
-    colum.innerHTML = `<th>${key}</th>`;
-    document.querySelector('.data-base').appendChild(colum);
-
-};  
-console.log(int);
-// for(let i = 0; i < ; i++){
-//     // console.log(Object.values(el).join(' '));
-//     console.log(i)
 // }
+// parse.forEach(function(element, index, array) {
+//     record(element, index);
+// });
+// });
 
-}
+// function record(el, int){
+// document.querySelector('.data-wrapper').innerHTML = `<table class="data-base"></table>`;
+// for(key in el){
+//     let colum = document.createElement('td');
+//     colum.innerHTML = `<th>${el[key]}</th>`;
+//     document.querySelector('.data-base').appendChild(colum);
+//     console.log(key)
+// }};  
+
+// for(key in el){
+//     let colum = document.createElement('td');
+//     colum.innerHTML = `<th>${el[key]}</th>`;
+//     document.querySelector('.data-base').appendChild(colum);
